9e0b10606dd574f9dd2008e8e12470d366b60b2c
Jonson-zhang <cdpf2008@gmail.com>
2026-06-26 06:37:16 +0000

    PM72114:37177: 162026n5 [06-2026年6月26日 14:37]

--- Boss直聘/test_env.js
+++ Boss直聘/test_env.js
@@ -11,46 +11,49 @@ global.document = {
         if (tag === 'iframe') {
             return {
                 style: {},
-                name: '',
-                src: '',
-                contentWindow: globalThis,  // 简化：直接指向 global
+                setAttribute: function() {},
+                getAttribute: function() { return null; },
+                contentWindow: globalThis,
             };
         }
         return { style: {}, setAttribute: function() {}, getAttribute: function() { return null; } };
     },
-    body: {
-        appendChild: function() {},
-    },
-    documentElement: {
-        appendChild: function() {},
-    },
+    body: { appendChild: function() {} },
+    documentElement: { appendChild: function() {} },
     getElementsByTagName: function(tag) {
-        return { item: function() { return null; } };
+        return { item: function() { return null; }, length: 0 };
     },
     cookie: '',
 };
 global.location = {
     hostname: 'www.zhipin.com',
     href: 'https://www.zhipin.com/web/geek/jobs',
-    protocol: 'https:',
     host: 'www.zhipin.com',
     pathname: '/web/geek/jobs',
-    search: '',
 };
 
 // 加载安全 JS
 try {
     console.log('[1] Loading security-7c91433f.js...');
-    require('fs').readFileSync(__dirname + '/config/security-7c91433f.js', 'utf8');
-    // If the file is eval'd and sets ABC on global
+    var code = require('fs').readFileSync(__dirname + '/config/security-7c91433f.js', 'utf8');
+    eval(code);
     console.log('[2] Done. Checking for ABC...');
     console.log('  typeof ABC:', typeof global.ABC);
-    console.log('  typeof window.ABC:', typeof window.ABC);
+
+    // Check for any new globals
+    var newGlobals = Object.getOwnPropertyNames(global).filter(function(k) {
+        return k.length <= 4 && k === k.toUpperCase() && typeof global[k] === 'function';
+    });
+    console.log('  Short uppercase function globals:', newGlobals);
+
     if (typeof global.ABC !== 'undefined') {
+        console.log('  ABC found!');
         console.log('  ABC keys:', Object.keys(global.ABC));
-        console.log('  ABC.prototype keys:', Object.keys(global.ABC.prototype || {}));
+        if (global.ABC.prototype) {
+            console.log('  ABC.prototype keys:', Object.keys(global.ABC.prototype));
+        }
     }
 } catch (e) {
     console.error('[!] Error:', e.message);
-    console.error('  Stack:', e.stack && e.stack.substring(0, 500));
+    console.error('  First 300 chars:', e.stack && e.stack.substring(0, 300));
 }
