6b5d9a3c2c1cadfad8cc4f63e83ce3f3157735c9
Jonson-zhang <cdpf2008@gmail.com>
2026-06-27 16:21:46 +0800

    知乎 iv8/: main_iv8.py移入iv8目录，模块完全自包含独立可拷走

--- 知乎/iv8/main_iv8.py
+++ 知乎/iv8/main_iv8.py
@@ -19,8 +19,6 @@ import urllib3
 
 urllib3.disable_warnings()
 
-# 添加 iv8 到 path
-sys.path.insert(0, str(Path(__file__).parent / "iv8"))
 from zhihu_sign import ZhihuSigner
 
 BASE_DIR = Path(__file__).parent
